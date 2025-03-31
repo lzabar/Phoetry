b06da302af475db46b01aeda0203fd186cce83db
Kodro23 <annekodro@gmail.com>
2025-03-31 12:42:14 +0200

    Ordered requirements. Updated main.py (reformatted image_recognition script). Creation of a train.py to train the poem generator

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-#Install required packages
+# Install required packages
 # pip install -r requirements.txt
 
-#Import libraries
-import os
-from src.image_recognition import image_label_detector, choose_label
+# Import libraries
+from src.image_recognition import SetClipModel
 from src.poem_generator import poem_generator
+
+# Define paths
+# project_folder_path="C:/Users/Annek/Documents/pytho exos/Phoetry/Phoetry_2"
+# images_path=project_folder_path+"/images"
+# model_path=project_folder_path+"/trained_model/poet-gpt2"
+
+
+# gpt-2
+import os
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-# Load model
+
 model_path = "trained_model/poet-gpt2"
 
 if not os.path.exists(os.path.join(model_path, "pytorch_model.bin")):
@@ -23,21 +32,32 @@ if not os.path.exists(os.path.join(model_path, "pytorch_model.bin")):
     print("Modèle téléchargé et sauvegardé !")
 else:
     print("Modèle déjà disponible, chargement en cours...")
-    model = GPT2LMHeadModel.from_pretrained(model_path)
-    tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 
+model = GPT2LMHeadModel.from_pretrained(model_path)
+tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+
+images_path = "images"
 
 # Proposed labels
-labels=["tree","flower","sunset","sunrise","cloud","mountain","beach","river","lake","waterfall","forest","grassland","desert","rain","snow",
-                   "road","traffic jam","hill","valley","cave","farm","garden","coastline","field","pond","sky","animal","insect","fungi",
-                   "leaf","pebble","stone", "dog", "cat","bird","butterfly","bee", "stars","moon","sun"]
+labels = [
+    "tree", "flower", "sunset", "sunrise", "cloud", "mountain", "beach", "river", "lake"
+    "waterfall", "forest", "grassland", "desert", "rain", "snow", "road", "traffic jam", "hill",
+    "valley", "cave", "farm", "garden", "coastline", "field", "pond", "sky", "animal", "insect",
+    "fungi", "leaf", "pebble", "stone", "dog", "cat", "bird", "butterfly",
+    "bee", "stars", "moon", "sun"
+]
 
 # Poem generation from picture
-image_path="images/sunset.jpg"
-def generate_poem_from_picture(image_path,labels):
-    top3_predicted_labels=image_label_detector(image_path,labels)
-    theme=choose_label(top3_predicted_labels)
-    poem=poem_generator(model_path=model_path, theme=theme, 
-          max_length=200,  temperature=0.5,  top_k=60, top_p=0.9, repetition_penalty=1.5)
+image_path = images_path + "/sunset.jpg"
+
+
+def generate_poem_from_picture(image_path, labels):
+    theme = SetClipModel().image_label_detector(image_path,labels)
+    poem = poem_generator(
+        model_path=model_path, theme=theme, max_length=200, temperature=0.5,
+        top_k=60, top_p=0.9, repetition_penalty=1.5
+    )
     return poem
-print(generate_poem_from_picture(image_path,labels))
+
+
+print(generate_poem_from_picture(image_path, labels))
--- src/image_recognition.py
+++ src/image_recognition.py
@@ -8,60 +8,53 @@ from tqdm import tqdm
 from transformers import CLIPProcessor, CLIPModel
 
 
-class IntializeClipModel:
+class SetClipModel:
     def __init__(self):
         self.model_id="openai/clip-vit-base-patch32"
         self.processor=CLIPProcessor.from_pretrained(self.model_id)
         self.model=CLIPModel.from_pretrained(self.model_id)
         self.device="cuda" if torch.cuda.is_available() else "cpu"
+     
         
 
-def create_label_tokens(labels):
-    """
-    Take the specific objects we want to identify in the image and create label tokens to feed the model
-    """
-    clip_model=IntializeClipModel()
-    #generate sentences
-    clip_labels=[f"a photo of a {label}" for label in labels ]
-    #Create label tokens
-    label_tokens=clip_model.processor(
-    text=clip_labels,
-    padding=True,
-    images=None,
-    return_tensors='pt'
-).to(clip_model.device)
-    #encode tokens to sentence embeddings
-    label_emb=clip_model.model.get_text_features(**label_tokens)
-#   detach from pytorch gradient computation
-    label_emb=label_emb.detach().cpu().numpy()
-    return label_emb
+    def create_label_tokens(self,labels):
+        """
+        Take the specific objects we want to identify in the image and create label tokens to feed the model
+        """
+        #generate sentences
+        clip_labels=[f"a photo of a {label}" for label in labels ]
+        #Create label tokens
+        label_tokens=self.processor(
+        text=clip_labels,
+        padding=True,
+        images=None,
+        return_tensors='pt'
+        ).to(self.model.device)
+        #encode tokens to sentence embeddings
+        label_emb=self.model.get_text_features(**label_tokens)
+        #detach from pytorch gradient computation
+        label_emb=label_emb.detach().cpu().numpy()
+        return label_emb
 
 
-#Predict label function
-def image_label_detector(image_path,labels):
-    """ 
-    Take the image path in input and predict the 3 most probable labels
-    """
-    clip_model=IntializeClipModel()
-    image = mpimg.imread(image_path)#Load image
-    processed_image=clip_model.processor( #Process image
-    text=None,
-    images=image,
-    return_tensors='pt'
-)["pixel_values"].to(clip_model.device)
-    image_emb=clip_model.model.get_image_features(processed_image).detach().cpu().numpy() #embedding
-    scores=np.dot(image_emb,create_label_tokens(labels).T) #predicted lables scores (among the 39 original labels)
-    top3_scores_indexes=list(np.argsort(scores)[0][-3:,][::-1])
-
-    return np.array(labels)[top3_scores_indexes].tolist()
-
-def choose_label(top3labels):
-    """ 
-    Randomly choose one label among the top 3 predicted labels
-    """
-    num_label=np.random.randint(0,3)
-    theme = top3labels[num_label]
-    return theme
+    #Predict label function
+    def image_label_detector(self,image_path,labels):
+        """ 
+        Take the image path in input and predict the 3 most probable labels
+        """
+        image = mpimg.imread(image_path)#Load image
+        processed_image=self.processor( #Process image
+        text=None,
+        images=image,
+        return_tensors='pt'
+        )["pixel_values"].to(self.model.device)
+        image_emb=self.model.get_image_features(processed_image).detach().cpu().numpy() #embedding
+        scores=np.dot(image_emb,SetClipModel().create_label_tokens(labels).T) #predicted lables scores (among the 39 original labels)
+        top3_scores_indexes=list(np.argsort(scores)[0][-3:,][::-1])
+        top3labels=np.array(labels)[top3_scores_indexes].tolist()
+        #choose label
+        num_label=np.random.randint(0,3)
+        return top3labels[num_label]
 
 
 
