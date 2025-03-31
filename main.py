67f00bf0d1678cfb3c05aa4d23a486b580967fef
Kodro23 <annekodro@gmail.com>
2025-03-31 08:52:42 +0200

    Download the poet-gpt2 model if not downloaded

--- main.py
+++ main.py
@@ -2,13 +2,30 @@
 # pip install -r requirements.txt
 
 #Import libraries
+import os
 from src.image_recognition import image_label_detector, choose_label
 from src.poem_generator import poem_generator
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+# Load model
+model_path = "trained_model/poet-gpt2"
 
-# Define paths
-project_folder_path="C:/Users/Annek/Documents/pytho exos/Phoetry/Phoetry_2"
-images_path=project_folder_path+"/images"
-model_path=project_folder_path+"/trained_model/poet-gpt2"
+if not os.path.exists(os.path.join(model_path, "pytorch_model.bin")):
+    print("Modèle non trouvé, téléchargement en cours...")
+    model_name = "gpt2"
+    # model = AutoModelForCausalLM.from_pretrained("gpt2")
+    # tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+    model.save_pretrained(model_path)
+    tokenizer.save_pretrained(model_path)
+    print("Modèle téléchargé et sauvegardé !")
+else:
+    print("Modèle déjà disponible, chargement en cours...")
+    model = GPT2LMHeadModel.from_pretrained(model_path)
+    tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+
 
 # Proposed labels
 labels=["tree","flower","sunset","sunrise","cloud","mountain","beach","river","lake","waterfall","forest","grassland","desert","rain","snow",
@@ -16,7 +33,7 @@ labels=["tree","flower","sunset","sunrise","cloud","mountain","beach","river","l
                    "leaf","pebble","stone", "dog", "cat","bird","butterfly","bee", "stars","moon","sun"]
 
 # Poem generation from picture
-image_path=images_path+"/sunset.jpg"
+image_path="images/sunset.jpg"
 def generate_poem_from_picture(image_path,labels):
     top3_predicted_labels=image_label_detector(image_path,labels)
     theme=choose_label(top3_predicted_labels)
