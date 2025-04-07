b7a7961a4ff5962c279640336c7872d5bf37685c
PaulToudret <paul-constant.toudret@ensae.fr>
2025-04-07 19:15:39 +0200

    enable set of clip model thanks to model_id

--- src/image_recognition.py
+++ src/image_recognition.py
@@ -1,3 +1,5 @@
+
+
 # Import libraries
 import torch
 import argparse
@@ -9,8 +11,26 @@ from transformers import CLIPProcessor, CLIPModel
 
 
 class SetClipModel:
-    def __init__(self):
-        self.model_id = "openai/clip-vit-base-patch32"
+    """
+    A class to set the model in charge of the
+    recognition of images.
+    This class is based on transformers
+    """
+    model_id : str
+    """The id of the model used"""
+    processor : CLIPProcessor
+    """The processor needed for the model, set with model_id"""
+    model : CLIPModel
+    """The model used based on the model_id, set with model_id"""
+    device : str
+    """Type of devide on with we used the model processor"""
+
+    def __init__(
+        self,
+        model_id : str = "openai/clip-vit-base-patch32"
+        ):
+        """Initialize the model thanks to model_id"""
+        self.model_id = model_id
         self.processor = CLIPProcessor.from_pretrained(self.model_id)
         self.model = CLIPModel.from_pretrained(self.model_id)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
