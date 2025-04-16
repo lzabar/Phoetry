9954c15d34312c97d5df139d7afc33a02832fc93
paultoudret <paul-constant.toudret@ensae.fr>
2025-04-16 21:58:46 +0000

    download model from S3 working

--- testing2.py
+++ testing2.py
@@ -5,12 +5,9 @@ Testing class LabelDico
 from src.label_dico import LabelDico
 
 
-
 labels = LabelDico()
 
 assert labels.URL == "https://minio.lab.sspcloud.fr/paultoudret/ensae-reproductibilite/Phoetry/Datasets/labels.json"
 assert labels.dico != {}
 
 print(labels.dico.keys())
-
-
